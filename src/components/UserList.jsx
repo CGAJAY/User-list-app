27c0b7050e5f3479568122068ef43c92e7927a30
CGAJAY <jmugendi737@gmail.com>
2024-11-10 16:34:01 +0300

    Styled the user list

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -19,23 +19,26 @@ const UserList = () => {
 	}, []);
 
 	return (
-		<ul>
-			{listOfUsers.map((user) => (
-				<li key={user.id}>
-					<h2>{user.name}</h2>
-					<p>
-						<strong>Username:</strong> {user.username}
-					</p>
-					<p>
-						<strong>Email:</strong> {user.email}
-					</p>
-					<p>
-						<strong>Address:</strong> {user.address.city},{" "}
-						{user.address.street}
-					</p>
-				</li>
-			))}
-		</ul>
+		<div className="user-list-container">
+			<h1 className="title">User List</h1>
+			<ul className="user-list">
+				{listOfUsers.map((user) => (
+					<li key={user.id} className="user-card">
+						<h2>{user.name}</h2>
+						<p>
+							<strong>Username:</strong> {user.username}
+						</p>
+						<p>
+							<strong>Email:</strong> {user.email}
+						</p>
+						<p>
+							<strong>Address:</strong> {user.address.city},{" "}
+							{user.address.street}
+						</p>
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 };
 
